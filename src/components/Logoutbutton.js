d70a69cf593476980becdf7bc84454f0d7319ace
Chaeyoung02 <chaedud02@naver.com>
2023-03-31 13:26:28 +0900

    LoginPage + MainPage + css + token + LogoutButton

---
+++ src/components/Logoutbutton.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
+const LogoutButton = () => {
+    const logout = async () => {
+
+        try {
+            const response = await fetch('/auth/logout', {
+                method: 'POST',
+                credentials:'include',
+            });
+            if(response.ok){
+                navigate('/auth/login');
+            } else {
+                throw new Error('로그아웃 실패');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+    const navigate = useNavigate;
+    return (
+        <button onClick={logout}>Logout</button>
+    );
+};
+export default LogoutButton;
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,12 @@
 //import { useHistory } from 'react-router-dom';
 import React from "react";
-
+import LogoutButton from '../components/Logoutbutton';
 export default function MainPage() {
-  /*const handleLogout = () => {
-    .get('auth/logout').then(response => {
-      console.log(response.data)
-    })
-    localStorage.removeItem('userID');
-    localStorage.removeItem('userPW');
 
-
-  }
-*/
   return (
     <div>
       <h2>메인 페이지</h2>
-      <button >로그아웃</button>
+      <LogoutButton/>
     </div>
   )
 }
