91d2cdb8f66646e463dda8e6991bf4ee6dde13e5
Chaeyoung02 <chaedud02@naver.com>
2023-04-05 15:10:17 +0900

    header + css + LogoutButton

--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -1,22 +1,22 @@
-import React ,{ useState} from "react";
+//import React, { useState } from "react";
 import LogoutButton from "../components/Logoutbutton";
+import "../style.css";
 
 export default function header() {
-    return( 
-        <div className="header">
-            <header>
-                <h1>LentalCare</h1>
-                <nav>
-                    <ul>
-                        <li><a href="{() => false}">홈</a></li>
-                        <li><a href="{() => false}">음주</a></li>
-                        <li><a href="{() => false}">흡연</a></li>
-                        <li><a href="{() => false}">충돌</a></li>
-                    </ul>
-                </nav>
-                <LogoutButton/>
-            </header>
+  return (
+    <div className="header">
+      <header>
+        <h1 className="main_logo">LentalCare</h1>
+        <div className="logout_button">
+          <LogoutButton />
         </div>
-
-    )
+        <nav className="nav">
+          <a href="{() => false}">홈</a>
+          <a href="{() => false}">음주</a>
+          <a href="{() => false}">흡연</a>
+          <a href="{() => false}">충돌</a>
+        </nav>
+      </header>
+    </div>
+  );
 }
